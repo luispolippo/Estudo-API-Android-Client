a6c5c9a8bd9e0fb3a56a2762182b592256cbced9
Luis Guilherme Polippo <luispolippo@hotmail.com>
2021-03-11 14:59:15 -0300

    created RecyclerView layout and the adapter

--- app/src/main/java/com/polippo/applicationcontentproviderclient/MainActivity.kt
+++ app/src/main/java/com/polippo/applicationcontentproviderclient/MainActivity.kt
@@ -2,10 +2,21 @@ package com.polippo.applicationcontentproviderclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var notesRecycler: RecyclerView
+    lateinit var notesRefreshButton: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notesRecycler = findViewById(R.id.client_list)
+        notesRefreshButton = findViewById(R.id.client_button_refresh)
+
+        notesRefreshButton.setOnClickListener {  }
     }
 }

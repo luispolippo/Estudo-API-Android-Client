8862283b937242256203c113f6908a17ea3aa8fc
Luis Guilherme Polippo <luispolippo@hotmail.com>
2021-03-11 15:26:35 -0300

    created and implemented ContentProvider

--- app/src/main/java/com/polippo/applicationcontentproviderclient/MainActivity.kt
+++ app/src/main/java/com/polippo/applicationcontentproviderclient/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.polippo.applicationcontentproviderclient
 
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +20,23 @@ class MainActivity : AppCompatActivity() {
 
         notesRecycler = findViewById(R.id.client_list)
         notesRefreshButton = findViewById(R.id.client_button_refresh)
+        getContentProvider()
 
-        notesRefreshButton.setOnClickListener {  }
+        notesRefreshButton.setOnClickListener { getContentProvider() }
+    }
+
+    private fun getContentProvider(){
+        try{
+            val url = "content://com.polippo.applicationcontentprovider.provider/notes"
+            val data = Uri.parse(url)
+            val cursor: Cursor? =
+                contentResolver.query(data, null, null, null, "title")
+            notesRecycler.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = ClientAdapter(cursor as Cursor)
+            }
+        } catch (ex: Exception){
+            ex.printStackTrace()
+        }
     }
 }
